d816addbb7545fd9f8514674a0754e362b40e78e
Falko Goettsch <falko@uw.edu>
2017-12-09 14:44:35 -0800

    better highlighting; demo ID; deprecate GREP_COLORS; prompt

--- regex_demo.sh
+++ regex_demo.sh
@@ -3,28 +3,56 @@
 # Apply a series of greps to input from STDIN
 # to demonstrate regular expressions
 
+DEFAULT_INFILE="./regex_demo.txt"
+
 # echo the string with line breaks
 IFS=""
+
+# use the grep alias inside the functions
+shopt -s expand_aliases
 
 # set default grep options
 # -n print line numbers
 # -E use extended regular expressions (egrep)
-export GREP_OPTIONS='--color=always -n -E'
-export GREP_COLORS='ms=01;31:mc=33:sl=:cx=:fn=01;37:ln=32:bn=35:se=36'
+alias grep='grep --color=always -n -E'
+export GREP_COLORS='ms=04;31:mc=33:sl=:cx=:fn=01;37:ln=32:bn=35:se=36'
 
+number=0
 function regex () {
-    echo "# grep ${@: 1:$(($# - 1))} /${!#}/"
-    echo $text | grep "$@|$"
+    ((number += 1))
+    echo "$number: grep ${@: 1:$(($# - 1))} /${!#}/"
+    #printf "%2d: grep %s /%s/\n" $number "${@: 1:$(($# - 1))}" ${!#}
+    tput setaf 2; echo $text | grep "$@|$"
     #echo $text | grep -v $@
     echo
+    prompt
 }
 
-text=`cat $1`
+function prompt() {
+    read -s -n 1 -p "Next (q to quit): " input
+    echo
+    echo
+
+    if [[ $input = "q" ]]; then
+        exit 0
+    fi
+}
+    
+if (( $# == 0 )); then
+    infile=$DEFAULT_INFILE
+else
+    infile=$1
+fi
+
+text=`cat $infile`
+
+clear
 
 # print original text
 echo "# original text"
 echo $text | grep '$'
 echo
+prompt
 
 # run the greps
 regex 'c'
@@ -52,3 +80,8 @@ regex '^$'
 regex '^.$'
 regex '^.*$'
 regex '^.*\.$'
+regex -i 'ide[ae]s'
+regex -i '([aeiou])\1'
+regex -i '[aeiou][aeiou]+'
+regex '[aeiou]{2,}'
+regex -v '[^aeiou ]{2,}'
