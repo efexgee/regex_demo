925dd0fabf84cf1e1a43a4876f40b1310162e671
Falko Goettsch <falko@uw.edu>
2017-12-10 22:26:34 -0800

    menu with colors; back; jump; back references fixed

--- regex_demo.sh
+++ regex_demo.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/bin/bash -f
+# set -f or glob expansion on echo will hide things like ".*"
 
-# Apply a series of greps to input from STDIN
+# Apply a series of greps to a file of text
 # to demonstrate regular expressions
 
+# The seq command is technically not standard
+if ! which seq > /dev/null; then
+    echo "Requires the seq command."
+    exit 10
+fi
+
+# enable shell pattern matching
+shopt -s extglob
+
+# default file to use as the input text if no filename is provided
 DEFAULT_INFILE="./regex_demo.txt"
 
 # determine whether to use the default text file
@@ -22,48 +33,7 @@ alias grep='grep --color=always -n -E'
 shopt -s expand_aliases
 export GREP_COLORS='ms=04;31:mc=33:sl=:cx=:fn=01;37:ln=32:bn=35:se=36'
 
-number=0
-function grep_it () {
-    ((number++))
-
-    clear
-    # match every line. using grep just to add the line numbers
-    echo "$text" | grep '$'
-    echo
-
-    # "-s" ignore lines without delimiters
-    local grep_args=`echo $1 | cut -s -d';' -f1`
-    # if we have args, pad them with a space
-    if [[ $grep_args != "" ]]; then
-        grep_args+=" "
-    fi
-    # without "-s" cut matches the whole line if there is no delimiter
-    local grep_regex=`echo $1 | cut -d';' -f2`
-
-    # print the header
-    echo "$number: grep $grep_args/`tput setaf 1`$grep_regex`tput sgr0`/"
-    prompt blank
-    # perform the grep
-    echo "$text" | grep $grep_args "($grep_regex|$)"
-    prompt
-}
-
-function prompt() {
-    if [[ $1 == "blank" ]]; then
-        read -s -n 1 input
-    else
-        echo
-        read -s -n 1 -p "Next (q to quit): " input
-        # one echo to cap the input
-        echo
-    fi
-    echo
-
-    if [[ $input = "q" ]]; then
-        exit 0
-    fi
-}
-
+# set up the regular expressions
 ORIG_IFS=$IFS
 IFS=$'\n'   # need this dollar or some escapes get evaluated in the strings
 regexes=(`cat <<EOF
@@ -99,9 +69,122 @@ ide[ae]s
 -v;[^aeiou ]{2,}
 EOF`
 )
-IFS=$ORIG_IFS
 
-# run the greps
-for regex in ${regexes[*]}; do
-    grep_it "$regex"
+# define some colors
+NORM=`tput sgr0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+BLUE=`tput setaf 4`
+
+# build a pattern to match menu replies
+choices=`seq -s '|' 1 ${#regexes[*]}`
+
+# build pretty version of regex list
+#   removes the ";" and adds color
+
+# find the longest entry to determine string padding
+max_length=$(($(echo "${regexes[*]}" | wc -L) - 3))
+
+for ((i = 0; i < ${#regexes[*]}; i++ )); do
+    grep_line=${regexes[$i]}
+
+    if echo $grep_line | grep -q ';'; then
+        # color codes, though not visible, make the strings too wide for "select"
+        #grep_args="${BLUE}$(echo $grep_line | cut -s -d';' -f1) ${NORM}"
+        grep_args="$(echo $grep_line | cut -d';' -f1) "
+    else
+        grep_args=""
+    fi
+    #grep_regex="/${RED}$(echo $grep_line | cut -d';' -f2)${NORM}/"
+    grep_regex="${RED}$(echo $grep_line | cut -d';' -f2)${NORM}"
+
+    pretty_regexes[$i]=$(printf "%-${max_length}s\n" "${grep_args}${grep_regex}")
+done
+
+# set up menu prompt text
+PS3=$'\n'"Choose regex (c)ontinue (q)uit: "
+
+function grep_it () {
+    local label=$1
+    local grep_line=$2
+
+    clear
+    # match every line. using grep just to add the line numbers
+    echo "$text" | grep '$'
+    echo
+
+    # "-s" ignore lines without delimiters
+    local grep_args=`echo $grep_line | cut -s -d';' -f1`
+    # if we have args, pad them with a space
+    local grep_spacer
+    if [[ $grep_args != "" ]]; then
+        local grep_spacer=" "
+    fi
+    # without "-s" cut matches the whole line if there is no delimiter
+    local grep_regex=`echo $grep_line | cut -d';' -f2`
+
+    # print the header
+    echo "$label: grep ${grep_args}${grep_spacer}/`tput setaf 1`$grep_regex`tput sgr0`/"
+    # wait for user
+    read -s -n 1 input
+    if [[ $input == "q" ]]; then
+        quitting
+    fi
+
+    echo
+    # print the grep output
+    # do not put "()" around the "|" or the back references break
+    echo "$text" | grep $grep_args "$grep_regex|$"
+    prompt
+}
+
+function quitting() {
+    # quits with status 0 because it was intentional
+    echo
+    echo "Quitting."
+    exit 0
+}
+
+function prompt() {
+    echo
+    read -s -n 1 -p "Next (b)ack (j)ump (m)enu (q)uit: " input
+    # one echo to cap the input
+    echo
+
+    case $input in
+        "q") quitting ;;
+        "j")
+            read -p "Jump to regex: " target_id
+            # subtract two from the target because:
+            #  the for loop will increment it
+            #  we were passed "id + 1"
+            case $target_id in
+                @($choices)) regex_id=$((target_id - 2)) ;;
+                *)
+                    echo
+                    read -s -n 1 -t 1 -p "Not a valid ID: $target_id" ;;
+            esac ;;
+        "b") regex_id=$((regex_id - 2)) ;;
+        "m") menu ;;
+        *) ;;
+    esac
+}
+
+function menu () {
+    clear
+    select choice in ${pretty_regexes[*]}; do
+        case $REPLY in
+            "c") break ;;
+            "q") quitting ;;
+            @($choices))
+                regex_id=$((REPLY -2))
+                break ;;
+            *) echo "Not a valid choice: $REPLY" ;;
+        esac 
+    done
+}
+
+# technically, this is the actual script
+for ((regex_id = 0; regex_id < ${#regexes[*]}; regex_id++)); do
+    grep_it $((regex_id + 1)) "${regexes[$regex_id]}"
 done
