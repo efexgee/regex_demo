d169801420874660a7eefd147c6dbd85479e18ac
Falko Goettsch <falko@uw.edu>
2018-01-31 20:55:49 -0800

    sed proof of concept

---
+++ sed_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+
+alias grep='grep --color=always -n -E'
+export GREP_COLORS='ms=04;31:mc=01;04;31:sl=:cx=01;30:fn=01;37:ln=32:bn=35:se=36'
+
+alias sed='sed -r'
+
+text=$(cat text)
+
+echo "$text"
+
+GREEN=`tput setaf 2`
+NORM=`tput sgr0`
+
+oIFS=$IFS
+
+IFS=''
+
+while true; do
+echo
+read -e -r -p "match: " match
+read -e -r -p "replace: " replace
+read -e -r -p "options: " options
+echo
+echo "/${match}/${replace}/"
+echo
+echo "$text" | grep -e '$' -e "$match"
+echo
+echo "$text" | sed "s/${match}/${GREEN}${replace}${NORM}/g${options}" | grep -e '$'
+echo
+read -s -n 1 -p "next"
+clear
+done
