a862bc78c620f5f17dea54a4ffb4fb3b26425030
Falko Goettsch <falko@uw.edu>
2018-02-01 20:59:07 -0800

    using bell as separator

--- sed_test.sh
+++ sed_test.sh
@@ -2,6 +2,8 @@
 
 shopt -s expand_aliases
 
+SEP=$'\a'
+
 alias grep='grep --color=always -n -E'
 export GREP_COLORS='ms=04;31:mc=01;04;31:sl=:cx=01;30:fn=01;37:ln=32:bn=35:se=36'
 
@@ -28,7 +30,7 @@ echo "/${match}/${replace}/"
 echo
 echo "$text" | grep -e '$' -e "$match"
 echo
-echo "$text" | sed "s~${match}~${GREEN}${replace}${NORM}~g${options}" | grep -e '$'
+echo "$text" | sed "s${SEP}${match}${SEP}${GREEN}${replace}${NORM}${SEP}g${options}" | grep -e '$'
 echo
 read -s -n 1 -p "next"
 clear
